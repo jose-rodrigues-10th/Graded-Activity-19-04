3696614edb5df5316189607499e60bc0f8a9b07d
mbecklas <75644072+mbecklas@users.noreply.github.com>
2021-04-17 13:32:33 -0300

    final version

---
+++ controlbuzzer_potenciometer.ino
@@ -0,0 +1,27 @@
+
+void setup(){
+  pinMode(A0, INPUT);
+  pinMode(8, OUTPUT);
+  Serial.begin(9600);
+
+
+}
+void loop()
+{
+  int var = analogRead(A0);
+  int y = map(var,0,1023,10000,1000);
+
+  Serial.println(y);
+  Serial.println(var);
+
+  if(var> 1000){
+    digitalWrite(8 ,HIGH);
+    noTone(2);
+  }else{
+    digitalWrite(8, LOW); 
+    tone(2,y);
+  }
+
+ 
+
+}
